2b76f0623c216162443e4d0aa0ce16f6685c1ee6
rig-release <rig@localhost>
2026-08-27 04:46:28 +0000

    rig 0.2.30

--- Formula/rig-cli.rb
+++ Formula/rig-cli.rb
@@ -3,8 +3,8 @@ class RigCli < Formula
 
   desc "Vehicle-level sensor-stack orchestrator (ROS 2 / Zenoh / Docker deployments)"
   homepage "https://github.com/christomaszewski/rig"
-  url "https://github.com/christomaszewski/rig/archive/refs/tags/v0.2.29.tar.gz"
-  sha256 "bfd9d8f27b0c237209585c52d99932a558b128abc9fa24945f9d5e4c080b2e49"
+  url "https://github.com/christomaszewski/rig/archive/refs/tags/v0.2.30.tar.gz"
+  sha256 "fbc57a432be77987f563fed2cfac8d783c4544a3d6318943a1e6d01c97fe984b"
 
   conflicts_with "rig", because: "homebrew-core's rig (an identity generator) also installs a `rig` executable"
 
